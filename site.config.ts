772dccd473c0c30a0ebc0d26a38c65b8b398b14b
Mike <59051453+manubu1012@users.noreply.github.com>
2024-04-14 10:40:33 +0800

    Update site.config.ts
    change basic information

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'stay with Mike',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Mike',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
